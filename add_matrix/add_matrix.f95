0ff2d08dd969c8cad6ede22991a7335a729a5e31
nit07 <nshukla@ist.utl.pt>
2020-04-09 18:40:21 +0200

    This is fortran advance_code

---
+++ add_matrix/add_matrix.f95
@@ -0,0 +1,63 @@
+! -------- Future Goal -----
+! I am going to parallize this code using MPI and OpenMP
+
+! psudo code
+! ========== 
+! User should give info about the size of matrix 
+! User should type number which will be equal to the size of matrix 
+! It will create matrix A and B 
+! then it will give output value C
+
+program add_matrix
+	!implicit none
+
+	integer :: i, j
+	INTEGER, PARAMETER :: n=3, m=3
+	real, dimension(n,m) :: a, b,c
+
+!----Computing time in second------!
+	 real time1
+     real time2
+     real ttime
+     call cpu_time(time1)
+
+	write(*,*) 'type the number 9 times for matrix A'
+	do i = 1, n
+	read(*,*)(a(i,j),j= 1,m)
+	end do 
+	write(*,*) 'Print matrix A'
+	do i = 1, n
+	print *, (a(i,j),j= 1,m)
+	end do 
+
+
+	write(*,*) 'type the number 9 times for matrix B'
+	do i = 1, n
+	read(*,*)(b(i,j),j= 1,m)
+	end do 
+	write(*,*) 'Print matrix B'
+	do i = 1, n
+	print *, (b(i,j),j= 1,m)
+	end do 
+
+	write(*,*) 'Sum C = matrix A + B'
+	do i = 1, n
+		do j = 1, m
+		c(i,j) = a(i, j) + b(i, j)
+		end do 
+	end do 
+
+	do i = 1, n
+		write(*,*)(c(i,j),j= 1, m)
+	end do 
+
+!----Computing time in second------!
+	call cpu_time( time2 )
+    ttime = time2 - time1
+    PRINT *, 'Elapsed time ', ttime ,' s' 
+
+end program add_matrix
+
+! I believe in open source do not bother to credit me 
+! Because I also leant from others 
+! If you find useful then just enjoy.
